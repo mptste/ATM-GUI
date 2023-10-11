5a87c7162314255f593415815a763a0444c6ab4e
mptste <124529697+mptste@users.noreply.github.com>
2023-10-11 12:21:32 +0200

    Update DBConnection.java

--- src/databaseConnection/DBConnection.java
+++ src/databaseConnection/DBConnection.java
@@ -3,30 +3,30 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package databaseConnection;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.*;
+
 /**
- *
  * @author JAVA4
  */
 public class DBConnection {
-    
-     private static Connection con = null;
-     
-  
-    static{ 
-        
+
+    private static Connection con = null;
+
+
+    static {
+
         try {
-           con = DriverManager.getConnection("jdbc:mysql://localhost:3306/atmguidb", "root","");
-        }
-        catch (SQLException e) {
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/atmguidb", "root", "");
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
-    public static Connection getConnection()
-    {
+
+    public static Connection getConnection() {
         return con;
     }
 }
